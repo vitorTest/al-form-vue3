8b3f73987711d77fee08b1cafe89ac5c2f68128d
Vitor-Felix <vytorfelix@gmail.com>
2022-07-11 01:45:50 -0300

    Refatorando/Evoluindo o Formulario/Temporizador

--- 01-explorando-framework/01-alura-tracker/src/App.vue
+++ 01-explorando-framework/01-alura-tracker/src/App.vue
@@ -1,26 +1,27 @@
 <template>
   <main class="columns is-gapless is-multiline">
-    <div class="column is-one-quarter ">
+    <div class="column is-one-quarter">
       <BarraLateral />
     </div>
-    <div class="column is-three-quarter ">
+    <div class="column is-three-quarter">
       <FormularioTarefa />
     </div>
   </main>
 </template>
 
 <script lang="ts">
-import BarraLateral from './components/BarraLateral.vue'
-import FormularioTarefa from './components/FormularioTarefa.vue'
+import { defineComponent } from "vue";
 
-export default {
-    name: "App",
-    components: { 
-      BarraLateral,
-      FormularioTarefa 
-    }
-}
+import BarraLateral from "./components/BarraLateral.vue";
+import FormularioTarefa from "./components/FormularioTarefa.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    BarraLateral,
+    FormularioTarefa,
+  },
+});
 </script>
 
-<style>
-</style>
+<style></style>
--- 01-explorando-framework/01-alura-tracker/src/components/FormularioTarefa.vue
+++ 01-explorando-framework/01-alura-tracker/src/components/FormularioTarefa.vue
@@ -1,40 +1,34 @@
 <template>
   <div class="box">
     <div class="columns">
-      <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
-        <input 
-          type="text" 
-          class="input" 
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
           placeholder="Qual tarefa você deseja iniciar?"
         />
-      </div>      
-        <div class="column">
-          <div class="is-flex is-align-items-center is-justify-content-space-between">
-            <section>
-              <strong>00:00:00</strong>
-            </section>
-            <button class="button">
-              <span class="icon">
-                <i class="fas fa-play"></i>
-              </span>
-              <span>play</span>
-            </button>
-            <button class="button">
-              <span class="icon">
-                <i class="fas fa-stop"></i>
-              </span>
-              <span>stop</span>
-            </button>
-          </div>
-        </div>    
+      </div>
+      <div class="column">
+        <Temporizador />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { defineComponent } from "vue";
 
-export default {
-  name: "FormularioTarefa"
-}
+import Temporizador from "./Temporizador.vue";
+
+export default defineComponent({
+  name: "FormularioTarefa",
+
+  components: {
+    Temporizador,
+  },
+});
 </script>
-
