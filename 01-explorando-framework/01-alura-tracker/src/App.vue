57c206e770cf222be7f032f836e8ab5b5ef1d9f6
Vitor-Felix <vytorfelix@gmail.com>
2022-07-10 23:19:18 -0300

    css externo e componentes

---
+++ 01-explorando-framework/01-alura-tracker/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter ">
+      <BarraLateral />
+    </div>
+    <div class="column is-three-quarter ">
+      <FormularioTarefa />
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import BarraLateral from './components/BarraLateral.vue'
+import FormularioTarefa from './components/FormularioTarefa.vue'
+
+export default {
+    name: "App",
+    components: { 
+      BarraLateral,
+      FormularioTarefa 
+    }
+}
+</script>
+
+<style>
+</style>
